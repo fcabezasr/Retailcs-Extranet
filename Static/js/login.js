28af1aa046fc16e5a0bd8cf8861845599407d586
fcabezasr <frankjesus21@gmail.com>
2017-02-13 16:07:58 -0500

    Fcabezasr - Changes 1 - Extranet
    Ya valida e inicia sessión en el Login y Logout!

--- Static/js/login.js
+++ Static/js/login.js
@@ -1,5 +1,71 @@
 $(document).ready(function(){
 
+	$('#login-ingresar').on('click', function(){
+
+		$(".error").fadeOut().remove();
+		if ($("#usuario").val() == "") {
+            $("#usuario").focus().after('<span class="error">Ingrese su usuario</span>');  
+            return false;
+        }
+        if ($("#usuario").val().length < 3) {
+            $("#usuario").focus().after('<span class="error">Ingrese un usuario correcto</span>');  
+            return false;
+        }
+		if ($("#contrasena").val() == "") {
+            $("#contrasena").focus().after('<span class="error">Ingrese su contraseña</span>');  
+            return false;
+        }
+
+		var cont = 0;
+        $(".error").each(function(index, element){ cont++; });
+
+        if(cont <= 0){
+        	var usuario = $('#usuario').val(), 
+        	contrasena = $('#contrasena').val();
+
+			$.ajax({
+				async: true,
+				url: 'iniciarSession/'+usuario+'/'+contrasena+'/',
+				cache: false,
+				data: { },
+				type: 'POST',
+				beforeSend : function(){
+
+				},
+				success : function(data){
+					
+					console.log(data);
+					if (data==1) {
+						location.href = '../';
+					}
+				},
+				error : function(xhr, status){
+
+				},
+				complete: function(xhr, status){
+
+				}
+			});        	
+        }
+
+		return false;
+	});
+
+
+    $("#usuario").bind('blur keyup', function(){
+        if ($(this).val() != "") {              
+            $('.error').fadeOut();
+            return false;  
+        }
+    });
+
+
+    $("#contrasena").bind('blur keyup', function(){
+        if ($(this).val() != "") {              
+            $('.error').fadeOut();
+            return false;  
+        }
+    });
 
 
 });
--- Static/js/retailcs.js
+++ Static/js/retailcs.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+	$('.net-servicio').on('click', function(){
+
+		$('.child_menu li').removeClass('active');
+		$(this).parent().addClass('active');
+
+		var page = $(this).attr('page');
+		var ser = $(this).attr('ser');
+
+		$('#principal').html(page+' '+ser);
+
+		return false;
+	});
+
 
 
 });
