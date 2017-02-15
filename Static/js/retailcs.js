2528b962f51dac2b12e1d64790aaa63239993a2e
fcabezasr <frankjesus21@gmail.com>
2017-02-15 16:03:40 -0500

    Fcabezasr - Changes 2 - Extranet
    Las vistas: Actualización, Corrección y Manual, ya están implementadas a
    nivel FrontEnd y parte del BackEnd. :)

--- Static/js/retailcs.js
+++ Static/js/retailcs.js
@@ -5,14 +5,65 @@ $(document).ready(function(){
 		$('.child_menu li').removeClass('active');
 		$(this).parent().addClass('active');
 
-		var page = $(this).attr('page');
-		var ser = $(this).attr('ser');
+		var pagina = $(this).attr('page');
+		var servicio = $(this).attr('ser');
 
-		$('#principal').html(page+' '+ser);
+		//$('#principal').html(page+' '+ser);
+		//$('#principal').css('background', 'blue');
+
+		$.ajax({
+			async: true,
+			url: 'extranet/page/service/'+pagina+'/'+servicio+'/',
+			cache: false,
+			data: {},
+			type: 'GET',
+			beforeSend: function(){
+
+			},
+			success: function(data){
+				$('#principal').html(data);
+			},
+			error: function(xhr, status){
+				alert('Ha ocurrido un error...');
+				console.log(status);
+				console.log(xhr);
+			},
+			complete: function(xhr, status){
+
+			}
+		});
 
 		return false;
 	});
 
+	$(document).on('click', '.close-link', function(){
+		var a=$(this).closest(".x_panel");
+		a.remove();
+	});
 
+	$(document).on('click', '.collapse-link', function(){
+		var a=$(this).closest(".x_panel"), b=$(this).find("i"), c=a.find(".x_content");
+		a.attr("style")?c.slideToggle(200,function(){a.removeAttr("style")}):(c.slideToggle(200),a.css("height","auto")),b.toggleClass("fa-chevron-up fa-chevron-down");
+	});
+
+	$(document).on('click', '.collapse-list', function(){
+		var a = $('.col-md-55');
+		a.css('width', '100%');
+		a.find('.view-list').css('display', 'none');
+		a.find('.caption').css('text-align', 'left');
+		a.find('.caption-left').css({'display': 'block', 'float' : 'left'});
+		a.find('.caption-right').css({'display': 'block', 'float' : 'right'});
+		a.find('.br-list').css('display','none');
+	});
+
+	$(document).on('click', '.collapse-grilla', function(){
+		var a = $('.col-md-55');
+		a.css('width', '20%');
+		a.find('.view-list').css('display', 'block');
+		a.find('.caption').css('text-align', 'center');
+		a.find('.caption-left').css({'display': 'block', 'float' : 'none'});
+		a.find('.caption-right').css({'display': 'block', 'float' : 'none'});
+		a.find('.br-list').css('display','block');
+	});
 
 });
