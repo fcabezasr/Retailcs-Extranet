3eda514bf136d530d8e1a971a337b0b36f84bab2
Henry <haruhi_01@hotmail.com>
2017-02-16 18:13:28 -0500

    HT - Changes 4 - Extranet
    login.phtml
    login.js
    uncompressed custom.min.css

--- Static/js/login.js
+++ Static/js/login.js
@@ -50,6 +50,53 @@ $(document).ready(function(){
 
 		return false;
 	});
+	
+	
+	$('#recovery-enviar').on('click', function(){
+
+		$(".error").fadeOut().remove();
+		if ($("#usuario_recovery").val() == "") {
+            $("#usuario_recovery").focus().after('<span class="error">Ingrese su usuario</span>');  
+            return false;
+        }
+        if ($("#usuario_recovery").val().length < 3) {
+            $("#usuario_recovery").focus().after('<span class="error">Ingrese un usuario correcto</span>');  
+            return false;
+        }
+
+		var cont = 0;
+        $(".recovery_section .error").each(function(index, element){ cont++; });
+
+        if(cont <= 0){
+        	var usuario = $('#usuario').val();
+
+			$.ajax({
+				async: true,
+				url: 'iniciarSession/'+usuario,
+				cache: false,
+				data: { },
+				type: 'POST',
+				beforeSend : function(){
+
+				},
+				success : function(data){
+					
+					console.log(data);
+					if (data==1) {
+						location.href = '../';
+					}
+				},
+				error : function(xhr, status){
+
+				},
+				complete: function(xhr, status){
+
+				}
+			});        	
+        }
+
+		return false;
+	});
 
 
     $("#usuario").bind('blur keyup', function(){
@@ -67,12 +114,48 @@ $(document).ready(function(){
         }
     });
 
+
+    $(document).keypress(function(e) {
+	    if(e.which == 13) {
+	    	if(!$('.login_section').hasClass('hide'))
+	        	$('#login-ingresar').trigger( "click" );
+	        else
+	        	$('#recovery-enviar').trigger( "click" );
+	    }
+	});
+
+
     $('.flex-container').height( 
     	$(window).height() 
     );
 
+
     $('.reset_pass').click(function(){
-    	alert("falta mostrar contrasena");
+    	$('.recovery_section').removeClass('hide');
+    	$('.login_section').addClass('hide');
     });
+
+
+    $('.login_enter').click(function(){
+    	$('.login_section').removeClass('hide');
+    	$('.recovery_section').addClass('hide');
+    });
+
+
+    $('.showpassword').click(function(){
+    	var contrasena = $('#contrasena');
+    	if(contrasena.attr('type') == 'text')
+    		contrasena.attr('type','password');
+    	else
+    		contrasena.attr('type','text');
+    });
+
+});
+
+$(window).resize(function(){
+
+	$('.flex-container').height( 
+    	$(window).height() 
+    );
 
 });
